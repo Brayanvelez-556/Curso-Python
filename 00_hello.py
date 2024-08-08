85e4e483540e0907c6a75e05315115762435c180
Brayan Velez <brayanvelez.556@gmail.com>
2024-08-08 16:49:16 -0500

    Dia 1 del curso:  Se aprendio sobre tipos de datos, impresiones, comentarios, variables y tipos de variables.

---
+++ 00_hello.py
@@ -0,0 +1,27 @@
+# Esto es un comentario
+# Nuestro primer comentario en Python
+
+print("Hola Python")
+print('Hola Python')
+
+"""
+Esto es un
+comentario 
+en varias lineas
+"""
+
+'''
+Esto tambien 
+es un 
+comentario en varias lienas
+'''
+
+# Consultar el tipo de dato
+print(type('Hola Python')) # Tipo de dato 'STR'
+print(type(5)) # Tipo de dato 'INT'
+print(type(5.7)) # Tipo de dato 'FLOAT'
+print(type(True)) # Tipo de dato 'BOOL'
+print(type(3 + 1J)) # Tipo de dato 'COMPLEX'
+
+print(type(print("Mi cadena de texto"))) # Tipo de dato 'NoneType'
+
---
+++ 01_variables.py
@@ -0,0 +1,34 @@
+# Variables
+
+my_string_variable = 'My string variable'
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+# Convertir una variable de tipo INT a tipo STR
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable) 
+
+# Concatenacion de variables en un print
+print(my_string_variable, ',', my_int_variable, ',', type(my_int_to_str_variable), ',', my_bool_variable)
+
+# Algunas Funciones del sistema
+print(len(my_string_variable)) # Len imprime la cantidad de caracteres que hay en el STR
+
+# Variables en una sola linea
+name, surname, alias, edad = "Brayan", "Velez", "Nando", 26
+print(" Mi nombre es:", name, "\n", "Mi apellido es:", surname, "\n", "Mi alias es:", alias, "\n", "Mi edad es:", edad)
+
+
+
+first_name = input("Cual es tu nombre: ")
+age = input("Cual es tu edad: ")
+
+print("Hola", first_name, "bienvenido, tu edad es:", str(age))
+
+
